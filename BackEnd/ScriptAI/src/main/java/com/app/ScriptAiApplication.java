282373fa395da8224a4fc3763e62ec397cc33b11
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-07-27 23:50:41 +0530

    openai-api-integrated-with-java-spring-boot-appilcation

---
+++ BackEnd/ScriptAI/src/main/java/com/app/ScriptAiApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptAiApplication.class, args);
+	}
+
+}
