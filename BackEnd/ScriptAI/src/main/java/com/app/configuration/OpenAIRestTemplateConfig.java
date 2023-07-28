d10d2e156107209a3d1823e97dd38677f969cc95
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-07-28 11:04:18 +0530

    bug-resolved

--- BackEnd/ScriptAI/src/main/java/com/app/configuration/OpenAIRestTemplateConfig.java
+++ BackEnd/ScriptAI/src/main/java/com/app/configuration/OpenAIRestTemplateConfig.java
@@ -15,17 +15,11 @@ public class OpenAIRestTemplateConfig {
     @Bean
     @Qualifier("openaiRestTemplate")
     public RestTemplate openaiRestTemplate() {
-    	
         RestTemplate restTemplate = new RestTemplate();
-        
         restTemplate.getInterceptors().add((request, body, execution) -> {
-        	
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
-            
             return execution.execute(request, body);
-            
         });
-        
         return restTemplate;
     }
 }
