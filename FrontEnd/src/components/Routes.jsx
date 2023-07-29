b981d847d08ee963be467803077fedc734852103
Rohit1438 <rohitbanerjee202@gmail.com>
2023-07-29 11:52:09 +0530

    recording and converting to text feature added

--- FrontEnd/src/components/Routes.jsx
+++ FrontEnd/src/components/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {  Routes, Route } from "react-router-dom";
 import { AllRoutes } from './allRoutes';
 import { Score } from './Score';
+import VoiceToText from './SpeechToText';
 export const RoutesA = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<AllRoutes/>}/>
             <Route path='/score' element={<Score/>}/>
+            <Route path='/voice' element={<VoiceToText/>}/>
         </Routes>
         
     </div>
