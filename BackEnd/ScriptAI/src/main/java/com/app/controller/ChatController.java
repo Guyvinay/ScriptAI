b134529951ffa819bffb3f6ce786acebe377f8f2
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-07-31 21:38:38 +0530

    some-minor-improvement

--- BackEnd/ScriptAI/src/main/java/com/app/controller/ChatController.java
+++ BackEnd/ScriptAI/src/main/java/com/app/controller/ChatController.java
@@ -22,33 +22,33 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 @CrossOrigin("*")
 public class ChatController {
+	
     @Qualifier("openaiRestTemplate")
     @Autowired
     private RestTemplate restTemplate;
+    
     @Value("${openai.model}")
     private String model;
+    
     @Value("${openai.api.url}")
     private String apiUrl;
     
     @PostMapping("/chat")
     public Prompt chat(@RequestBody Prompt prompt , HttpSession session) {
-        // create a request
-    	
+     
     	List<Message> chatHistory = (List<Message>) session.getAttribute("chatHistory" );
         if (chatHistory == null) {
              chatHistory = new ArrayList<>();
         }
         chatHistory.add(new Message("user", prompt.getPromt()));
-//    	System.out.println(prompt);
+
         ChatRequest request = new ChatRequest(model, prompt.getPromt());
-        // call the API
-//        System.out.println("Hello");
+
         ChatResponse response = restTemplate.postForObject(apiUrl, request, ChatResponse.class);
         if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
             return null;
         }
-        // return the first response
-//        return new Prompt(response.getChoices().get(0).getMessage().getContent());
+        
         Prompt reply =new Prompt(response.getChoices().get(0).getMessage().getContent());
         
         session.setAttribute("chatHistory" , chatHistory);
